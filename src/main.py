88dbd44872171698cb258a79030749212771c0e7
smn.crm <smn.cramer@icloud.com>
2024-08-17 19:36:35 +0200

    add scrape_episodes

--- src/main.py
+++ src/main.py
@@ -1,7 +1,9 @@
-from scraper import scrape_tracks
+from scraper import scrape_episodes, scrape_tracks
 
 # Usage example
-url = "https://www.nts.live/shows/macca/episodes/macca-10th-august-2024"
-result = scrape_tracks(url)
+show_url = "https://www.nts.live/shows/macca"
+episodes = scrape_episodes(show_url)
+episode_url = "https://www.nts.live/shows/macca/episodes/macca-10th-august-2024"
+tracks = scrape_tracks(episode_url)
 
-print(result)
+print(episodes)
--- src/scraper.py
+++ src/scraper.py
@@ -1,9 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
+from urllib.parse import urljoin
 
 
 def scrape_tracks(url):
-    """Scrapes all tracks from an NTS show webpage"""
+    """Scrapes all tracks from an NTS episode."""
     response = requests.get(url)
 
     if response.status_code == 200:
@@ -19,6 +20,27 @@ def scrape_tracks(url):
             }
 
         return extracted_tracks
+    else:
+        print(f"Failed to retrieve the webpage. Status code: {response.status_code}")
+        return None
+
+def scrape_episodes(url):
+    """Scrapes all urls to individual episodes from an NTS show."""
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, "html.parser")
+        episodes = soup.find_all("div", class_="nts-grid-v2-item__content")
+
+        # Process and store the extracted data
+        extracted_episodes = {}
+        for ix, e in enumerate(episodes):
+            extracted_episodes[ix] = {
+                "title": e.find("div", class_="nts-grid-v2-item__header__title").text,
+                "url": urljoin(url, e.find("a", class_="nts-grid-v2-item__header nts-app")['href']),
+            }
+
+        return extracted_episodes
     else:
         print(f"Failed to retrieve the webpage. Status code: {response.status_code}")
         return None
