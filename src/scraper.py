e319f03a942132eefd6467ba3da924e8b0e9aaad
smn.crm <smn.cramer@icloud.com>
2024-08-17 19:18:51 +0200

    remove some comments, add docstring

--- src/scraper.py
+++ src/scraper.py
@@ -3,15 +3,11 @@ from bs4 import BeautifulSoup
 
 
 def scrape_tracks(url):
-    # Send a GET request to the URL
+    """Scrapes all tracks from an NTS show webpage"""
     response = requests.get(url)
 
-    # Check if the request was successful
     if response.status_code == 200:
-        # Parse the HTML content
         soup = BeautifulSoup(response.text, "html.parser")
-
-        # Find and extract specific elements
         tracks = soup.find_all("div", class_="track__detail")
 
         # Process and store the extracted data
