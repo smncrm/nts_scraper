234056c320d5bb34c5d1dac17d3700a5bda290dd
smn.crm <smn.cramer@icloud.com>
2024-09-04 14:45:29 +0200

    add Track class

---
+++ src/Track.py
@@ -0,0 +1,7 @@
+class Track:
+    def __init__(self, artist: str, title: str):
+        self.artist = artist
+        self.title = title
+
+    def set_year(self, year: str):
+        self.year = year
