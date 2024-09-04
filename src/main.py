4d3ea8b5cd646b25774483fe05dbae4a46bbdea0
smn.crm <smn.cramer@icloud.com>
2024-09-04 15:14:24 +0200

    add spotify search functionality

--- src/main.py
+++ src/main.py
@@ -1,9 +1,15 @@
 from scraper import scrape_episodes, scrape_tracks
+from spotify_service import SpotifyService
 
 # Usage example
 show_url = "https://www.nts.live/shows/macca"
-episodes = scrape_episodes(show_url, dynamic_page=True)
+# episodes = scrape_episodes(show_url, dynamic_page=False)
 episode_url = "https://www.nts.live/shows/macca/episodes/macca-10th-august-2024"
 tracks = scrape_tracks(episode_url)
 
-print(episodes)
+track_one = tracks[0]
+print(track_one)
+
+spotify_service = SpotifyService()
+
+print(spotify_service.search_track(track_one))
--- src/spotify_service.py
+++ src/spotify_service.py
@@ -1,35 +1,43 @@
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
-# Set up authentication
-scope = "playlist-modify-public playlist-modify-private"
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
+from Track import Track
 
 
-def get_user_playlists():
-    playlists = sp.current_user_playlists()
-    return {playlist["name"]: playlist["id"] for playlist in playlists["items"]}
+class SpotifyService:
+    def __init__(self):
+        scope = "playlist-modify-public playlist-modify-private"
+        self.sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
 
+    def get_user_playlists(self):
+        playlists = self.sp.current_user_playlists()
+        return {playlist["name"]: playlist["id"] for playlist in playlists["items"]}
 
-def create_playlist():
-    playlist_name = input("Enter the name of the playlist you want to add songs to: ")
+    def create_playlist(self):
+        playlist_name = input(
+            "Enter the name of the playlist you want to add songs to: "
+        )
 
-    user_playlists = get_user_playlists()
-    playlist_id = user_playlists.get(playlist_name)
+        user_playlists = self.get_user_playlists()
+        playlist_id = user_playlists.get(playlist_name)
 
-    if playlist_id:
-        print("Playlist existst already.")
-    else:
-        sp.user_playlist_create(user=sp.me()["id"], name=playlist_name)
-        return
+        if playlist_id:
+            print("Playlist existst already.")
+        else:
+            self.sp.user_playlist_create(user=self.sp.me()["id"], name=playlist_name)
+            return
 
-
-def search_track(track_name):
-    results = sp.search(q=track_name, type="track", limit=1)
-    sp.search()
-    if results["tracks"]["items"]:
-        return results["tracks"]["items"][0]["uri"]
-    return None
-
-
-create_playlist()
+    def search_track(self, track: Track):
+        query = f"artist:{track.artist} track:{track.title}"
+        results = self.sp.search(q=query, type="track", limit=1)
+        if results["tracks"]["items"]:
+            track = results["tracks"]["items"][0]
+            return {
+                "name": track["name"],
+                "artist": track["artists"][0]["name"],
+                "album": track["album"]["name"],
+                "id": track["id"],
+                "uri": track["uri"],
+            }
+        else:
+            return None
